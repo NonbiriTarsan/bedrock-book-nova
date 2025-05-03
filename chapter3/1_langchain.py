3df141ec8c0f9fcf6b806d767f8662b938097a9e
unknown <NonbiriTarsan>
2025-05-03 19:03:43 +0900

    20250503 commit

---
+++ chapter3/1_langchain.py
@@ -0,0 +1,25 @@
+# Pyhton外部モジュールのインポート
+from langchain_aws import ChatBedrock
+from langchain_core.messages import HumanMessage, SystemMessage
+
+
+
+# ChatBedrockを生成
+# モデルを定義（Amazon Nova Pro 東京リージョン）
+chat = ChatBedrock(
+    credentials_profile_name="admin", 
+    model_id = "apac.amazon.nova-pro-v1:0",
+    model_kwargs={"max_tokens": 1000},
+)
+
+# メッセージを定義
+messages = [
+    SystemMessage(content="あなたのタスクはユーザーの質問に明確に答えることです。"),
+    HumanMessage(content="空が青いのはなぜですか？"),
+]
+
+# モデル呼び出し
+response = chat.invoke(messages)
+
+
+print(response.content)
