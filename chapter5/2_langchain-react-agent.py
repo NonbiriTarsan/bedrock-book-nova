867f867accaaab52f06b2712c4b73ff78e060a8e
unknown <NonbiriTarsan>
2025-05-25 13:08:06 +0900

    20250525 commit

---
+++ chapter5/2_langchain-react-agent.py
@@ -0,0 +1,48 @@
+import nest_asyncio
+import streamlit as st
+from bs4 import BeautifulSoup
+from langchain import hub
+from langchain.agents import AgentExecutor, Tool, create_react_agent
+from langchain_aws import ChatBedrock
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_core.messages import HumanMessage, SystemMessage
+
+nest_asyncio.apply()
+
+search = DuckDuckGoSearchRun()
+tools = [
+    Tool(
+        name="duckduckgo-search",
+        func=search.run,
+        description="このツールはユーザーから検索キーワードを受け取り、Web上の最新情報を検索します。",
+    )
+]
+
+# チャットモデルの設定
+chat = ChatBedrock(
+    credentials_profile_name="admin", 
+    model_id = "apac.amazon.nova-pro-v1:0",
+    model_kwargs={"max_tokens": 1500},
+)
+
+
+# エージェントの設定
+agent = create_react_agent(chat, tools, prompt=hub.pull("hwchase17/react"))
+agent_executor = AgentExecutor(
+    agent=agent, tools=tools, verbose=True, handle_parsing_errors=True
+)
+
+# Streamlit アプリケーションの設定
+st.title("Bedrock ReAct Agent チャット")
+messages = [SystemMessage(content="あなたは質問に対して必ず日本語で回答します。")]
+
+# ユーザー入力の処理
+prompt = st.chat_input("何でも聞いてください。")
+if prompt:
+    messages.append(HumanMessage(content=prompt))
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    with st.chat_message("assistant"):
+        # エージェントを呼び出す
+        result = agent_executor.invoke({"input": prompt})
+        st.write(result["output"])
